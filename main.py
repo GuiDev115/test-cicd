93e7c0cd4532923a9e69623791e1fefc8b82b8b1
Guilherme Fabrício <43139846+GuiDev115@users.noreply.github.com>
2026-09-21 21:56:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
 # Solicita os dois números ao usuário
-num1 = float(input("Digite o primeiro número: "))
-num2 = float(input("Digite o segundo número: "))
-
-# Realiza a soma
-soma = num1 + num2
 
 # Exibe o resultado
-print(f"A soma de {num1} e {num2} é: {soma}")
+print(f"Ola, python funcionando")
